16f989e3abd1f7547889c4d06d58ee638ea9fbbb
informedcitizenry <informedcitizenry@gmail.com>
2017-09-15 13:45:58 -0700

    [ci skip]

--- NUnit.z80Tests/NUnitTestZ80Misc.cs
+++ NUnit.z80Tests/NUnitTestZ80Misc.cs
@@ -1,4 +1,5 @@
-﻿using z80DotNet;
+﻿using DotNetAsm;
+using z80DotNet;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -14,8 +15,8 @@ namespace NUnit.z80Tests
         [Test]
         public void TestFormatBuilder()
         {
-            z80DotNet.z80Asm.FormatBuilder builder =
-                new z80Asm.FormatBuilder(@"^\(\s*(hl)\s*\)\s*,\s*(.+)$()()",
+            FormatBuilder builder =
+                new FormatBuilder(@"^\(\s*(hl)\s*\)\s*,\s*(.+)$()()",
                                           "({0}),{2}{1}",
                                           "${0:x2}",
                                           string.Empty,
@@ -24,14 +25,14 @@ namespace NUnit.z80Tests
                                           2,
                                           4, false);
 
-            z80Asm.z80Format fmt = builder.GetFormat("( hl ) , $00");
+            OperandFormat fmt = builder.GetFormat("( hl ) , $00");
 
             Assert.IsNotNull(fmt);
-            Assert.AreEqual("(hl),${0:x2}", fmt.StringFormat);
+            Assert.AreEqual("(hl),${0:x2}", fmt.FormatString);
             Assert.AreEqual("$00", fmt.Expression1);
             Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression2));
 
-            builder = new z80Asm.FormatBuilder(@"^\(\s*i(x|y)\s*\+(.+)\)(\s*,\s*([a-ehl]))?$",
+            builder = new FormatBuilder(@"^\(\s*i(x|y)\s*\+(.+)\)(\s*,\s*([a-ehl]))?$",
                                                 "(i{0}+{2}){1}",
                                                 "${0:x2}",
                                                 string.Empty,
@@ -41,40 +42,54 @@ namespace NUnit.z80Tests
                                                 5, false);
             fmt = builder.GetFormat("(ix+$30),a");
             Assert.IsNotNull(fmt);
-            Assert.AreEqual("(ix+${0:x2}),a", fmt.StringFormat);
+            Assert.AreEqual("(ix+${0:x2}),a", fmt.FormatString);
             Assert.AreEqual("$30", fmt.Expression1);
 
             fmt = builder.GetFormat("(ix+$50)");
             Assert.IsNotNull(fmt);
-            Assert.AreEqual("(ix+${0:x2})", fmt.StringFormat);
+            Assert.AreEqual("(ix+${0:x2})", fmt.FormatString);
             Assert.AreEqual("$50", fmt.Expression1);
 
-            builder = new z80Asm.FormatBuilder(@"^(([a-ehl])\s*,\s*)?\(\s*(bc|de|hl|ix|iy)\s*\)$()", "{0}", string.Empty, string.Empty, 0, 2, 4, 4, false);
+            builder = new FormatBuilder(@"^(([a-ehl])\s*,\s*)?\(\s*(bc|de|hl|ix|iy)\s*\)$()", "{0}", string.Empty, string.Empty, 0, 2, 4, 4, false);
 
             fmt = builder.GetFormat("(hl)");
             Assert.IsNotNull(fmt);
-            Assert.AreEqual("(hl)", fmt.StringFormat);
+            Assert.AreEqual("(hl)", fmt.FormatString);
             Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression1));
             Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression2));
 
             fmt = builder.GetFormat("a , ( de )");
             Assert.IsNotNull(fmt);
-            Assert.AreEqual("a,(de)", fmt.StringFormat);
+            Assert.AreEqual("a,(de)", fmt.FormatString);
             Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression1));
             Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression2));
 
-            builder = new z80Asm.FormatBuilder(@"^(bc|de|hl|ix|iy|sp)\s*,\s*(.+)$()", "{0},{2}", "${0:x4}", string.Empty, 1, 3, 2, 3, false, true);
+            builder = new FormatBuilder(@"^(bc|de|hl|ix|iy|sp)\s*,\s*(.+)$()", "{0},{2}", "${0:x4}", string.Empty, 1, 3, 2, 3, false, true);
             
             fmt = builder.GetFormat("hl,$0000");
             Assert.IsNotNull(fmt);
-            Assert.AreEqual("hl,${0:x4}", fmt.StringFormat);
+            Assert.AreEqual("hl,${0:x4}", fmt.FormatString);
             Assert.AreEqual("$0000", fmt.Expression1);
             Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression2));
 
             fmt = builder.GetFormat("hl , ( $0000 )");
             Assert.IsNotNull(fmt);
-            Assert.AreEqual("hl,(${0:x4})", fmt.StringFormat);
+            Assert.AreEqual("hl,(${0:x4})", fmt.FormatString);
             Assert.AreEqual("( $0000 )", fmt.Expression1);
+            Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression2));
+
+            builder = new FormatBuilder(@"^(.+)\s*,\s*y$()", "{2},y", "${0:x4}", string.Empty, 2, 2, 1, 2, false, true);
+
+            fmt = builder.GetFormat("$0000 , y");
+            Assert.IsNotNull(fmt);
+            Assert.AreEqual("${0:x4},y", fmt.FormatString);
+            Assert.AreEqual("$0000", fmt.Expression1.Trim());
+            Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression2));
+
+            fmt = builder.GetFormat("(  ZP_VAR  ),y");
+            Assert.IsNotNull(fmt);
+            Assert.AreEqual("(${0:x4}),y", fmt.FormatString);
+            Assert.AreEqual("(  ZP_VAR  )", fmt.Expression1);
             Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression2));
         }
 
@@ -83,33 +98,33 @@ namespace NUnit.z80Tests
         {
             TestController controller = new TestController(null);
 
-            z80Asm.FormatBuilder builder = new z80Asm.FormatBuilder(@"^\(\s*(c)\s*\)\s*,\s*(.+)$()", "({0}),{3}", string.Empty, "{0}", 1, 3, 3, 2, controller.Options.CaseSensitive, controller.Evaluator);
+            FormatBuilder builder = new FormatBuilder(@"^\(\s*(c)\s*\)\s*,\s*(.+)$()", "({0}),{3}", string.Empty, "{0}", 1, 3, 3, 2, controller.Options.CaseSensitive, controller.Evaluator);
 
-            z80Asm.z80Format fmt = builder.GetFormat("(c),0");
+            OperandFormat fmt = builder.GetFormat("(c),0");
             Assert.IsNotNull(fmt);
-            Assert.AreEqual("(c),0", fmt.StringFormat);
+            Assert.AreEqual("(c),0", fmt.FormatString);
             Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression1));
             Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression2));
 
             fmt = builder.GetFormat("( c  ), ( 60 - 60)");
             Assert.IsNotNull(fmt);
-            Assert.AreEqual("(c),0", fmt.StringFormat);
+            Assert.AreEqual("(c),0", fmt.FormatString);
             Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression1));
             Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression2));
 
-            builder = new z80Asm.FormatBuilder(@"^(.+)\s*,\s*\(\s*i(x|y)\s*((\+|-).+)\)(\s*,\s*[a-ehl])?$()", "{3},(i{0}+{2}){1}", "${0:x2}", "{0}", 2, 5, 3, 1, controller.Options.CaseSensitive, controller.Evaluator);
+            builder = new FormatBuilder(@"^(.+)\s*,\s*\(\s*i(x|y)\s*((\+|-).+)\)(\s*,\s*[a-ehl])?$()", "{3},(i{0}+{2}){1}", "${0:x2}", "{0}", 2, 5, 3, 1, controller.Options.CaseSensitive, controller.Evaluator);
 
             fmt = builder.GetFormat("0,(ix+$30),a");
             Assert.IsNotNull(fmt);
-            Assert.AreEqual("0,(ix+${0:x2}),a", fmt.StringFormat);
+            Assert.AreEqual("0,(ix+${0:x2}),a", fmt.FormatString);
             Assert.AreEqual("+$30", fmt.Expression1);
             Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression2));
 
-            builder = new z80Asm.FormatBuilder(@"^\(\s*i(x|y)\s*\+(.+)\)\s*,\s*(.+)$()", "(i{0}+{2}),{3}", "${0:x2}", "${1:x2}", 1, 4, 2, 3, controller.Options.CaseSensitive);
+            builder = new FormatBuilder(@"^\(\s*i(x|y)\s*\+(.+)\)\s*,\s*(.+)$()", "(i{0}+{2}),{3}", "${0:x2}", "${1:x2}", 1, 4, 2, 3, controller.Options.CaseSensitive);
 
             fmt = builder.GetFormat("(ix+$00),$ff");
             Assert.IsNotNull(fmt);
-            Assert.AreEqual("(ix+${0:x2}),${1:x2}", fmt.StringFormat);
+            Assert.AreEqual("(ix+${0:x2}),${1:x2}", fmt.FormatString);
             Assert.AreEqual("$00", fmt.Expression1);
             Assert.AreEqual("$ff", fmt.Expression2);
         }
